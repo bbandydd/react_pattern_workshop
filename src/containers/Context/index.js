7d2dcdf45bd7d164c57d1124820bf6b1d2a2ca07
Andy Tsai <bbandydd@gmail.com>
2018-12-12 13:26:09 +0800

    context add description

--- src/containers/Context/index.js
+++ src/containers/Context/index.js
@@ -5,10 +5,10 @@ import Ver2 from './Ver2';
 export default () => {
   return (
     <React.Fragment>
-      <h2>Ver1</h2>
+      <h2>Ver1: prop drilling</h2>
       <Ver1/>
 
-      <h2>Ver2</h2>
+      <h2>Ver2: 使用React Context API修改</h2>
       <Ver2 />
     </React.Fragment>
   )
