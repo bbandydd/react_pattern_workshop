f4656ecba39a80cd21de0570f29abc186b0b135c
Andy Tsai <bbandydd@gmail.com>
2018-12-18 23:29:36 +0800

    fix state initializer error

--- src/containers/StateInitializers/index.js
+++ src/containers/StateInitializers/index.js
@@ -13,20 +13,12 @@ export default () => {
       <h2>Ver2: 加入Reset初始化狀態</h2>
       <li>新增initialState並給state初始化、this.reset使用</li>
       <hr/>
-      <Ver2>
-        <Ver2.On>The button is on</Ver2.On>
-        <Ver2.Off>The button is off</Ver2.Off>
-        <Ver2.Button />
-      </Ver2>
+      <Ver2 />
 
       <h2>Ver3: 使用者自己定義初始狀態</h2>
       <li>提供props - initialOn給使用者自定義初始化狀態值</li>
       <hr/>
-      <Ver3 initialOn>
-        <Ver3.On>The button is on</Ver3.On>
-        <Ver3.Off>The button is off</Ver3.Off>
-        <Ver3.Button />
-      </Ver3>
+      <Ver3 initialOn />
     </React.Fragment>
   )
 }
